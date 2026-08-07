3d1db1c2ed6027e20a97b469474308d27fd2e7e0
Laaaaaaaam <2667605815@qq.com>
2026-08-07 21:14:30 +0800

    版本号同步 v0.5.0

--- codemap/__init__.py
+++ codemap/__init__.py
@@ -30,7 +30,7 @@ from codemap.resolver import Resolver
 from codemap.build import build
 from codemap.query import Querier
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = [
     "Node",
     "Edge",
